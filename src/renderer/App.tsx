715ed34f6073673b6fbe7d03568a9e80d8868007
moxun <1786471868@qq.com>
2026-03-02 15:38:22 +0800

    fix: 全屏模式下聊天面板添加关闭按钮

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -176,7 +176,7 @@ const App: React.FC = () => {
             className={`w-1 h-full bg-gray-700 hover:bg-primary-500 cursor-col-resize transition-colors float-left ${isDragging ? 'bg-primary-500' : ''}`}
           />
           <div className="h-full float-right" style={{ width: chatWidth - 4 }}>
-            <ChatPanel width={chatWidth - 4} />
+            <ChatPanel width={chatWidth - 4} onClose={toggleChatPanel} />
           </div>
         </div>
 
